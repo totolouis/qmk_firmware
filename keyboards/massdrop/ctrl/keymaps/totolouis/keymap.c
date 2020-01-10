3020b3ffbf14af9010f6ff2a392ef7759db87d97
Louis <louismagand@gmail.com>
2020-01-09 20:58:59 -0800

    return 5

--- keyboards/massdrop/ctrl/keymaps/totolouis/keymap.c
+++ keyboards/massdrop/ctrl/keymaps/totolouis/keymap.c
@@ -361,6 +361,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                         send_altcode(249, record); // u
                     }
                 }
+                else {
+                    return true;
+                }
                 return false;
                 break;
     		case ACT_E2:
